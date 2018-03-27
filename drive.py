6618a3374adc8be484c0d1df0f52035c666862c0
prathmesh-dali <prathmesh.dali@gmail.com>
2018-03-28 02:20:52 +0530

    Second Commit

--- drive.py
+++ drive.py
@@ -2,8 +2,8 @@ import argparse
 import base64
 from datetime import datetime
 import os
-import shutil
 import cv2
+import shutil
 
 import numpy as np
 import socketio
@@ -62,7 +62,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        image_array = cv2.resize(image[60:140,:], (64,64))
+        image_array = cv2.resize(image_array[60:140,:], (64,64))
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
--- model.py
+++ model.py
@@ -1,17 +1,21 @@
 import csv
 import cv2
 import numpy as np
+import datetime
+import random
 from sklearn.utils import shuffle
 lines = []
 
 X_train = []
 y_train = []
 
+print('start', datetime.datetime.now())
 with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
+print('read lines', datetime.datetime.now())
 
 images_center = []
 images_left = []
@@ -33,6 +37,9 @@ for line in lines:
 
 len_left, len_right, len_center = len(images_left), len(images_right), len(images_center)
 
+print('initial right left', datetime.datetime.now())
+
+
 while len_center - len_left > 0:
     rand = int(np.random.choice(len(lines),1))
     line = lines[rand]
@@ -41,6 +48,9 @@ while len_center - len_left > 0:
         images_left.append(cv2.imread('./data/IMG/'+ line[2].split('/')[-1]))
         measurements_left.append(measurement)
     len_left = len(images_left)
+
+print('left', datetime.datetime.now())
+
 
 while len_center - len_right > 0:
     rand = int(np.random.choice(len(lines),1))
@@ -51,8 +61,11 @@ while len_center - len_right > 0:
         measurements_right.append(measurement)
     len_right = len(images_right)
 
-X_train = np.array(images_center+images_left+images_right)
-y_train = np.array(measurements_center+measurements_left+measurements_right)
+print('right', datetime.datetime.now())
+
+
+X_train = images_center+images_left+images_right
+y_train = measurements_center+measurements_left+measurements_right
 
 def crop_and_resize(image):
     image = cv2.resize(image[60:140,:], (64,64))
@@ -71,8 +84,8 @@ def generator_images(batchSize = 32):
         images, angle = shuffle(X_train, y_train)
         for i in range(batchSize):
             choice = int(np.random.choice(len(images),1))
-            X_batch[i] = randomize_brightness(crop_and_resize(images[choice]))
-            y_batch[i] = angle[choice]
+            X_batch.append(randomize_brightness(crop_and_resize(images[choice])))
+            y_batch.append(angle[choice])
             if(random.randint(0,1)):
                 X_batch[i] = np.fliplr(X_batch[i])
                 y_batch[i] = - y_batch[i]
@@ -84,39 +97,35 @@ def generator_valid(batchSize = 32):
     while True:
         X_valid = []
         y_valid = []
+        images, angle = shuffle(X_train, y_train)
         for i in range(batchSize):
             choice = int(np.random.choice(len(images),1))
-            X_valid[i] = randomize_brightness(crop_and_resize(images[choice]))
-            y_valid[i] = angle[choice]
-        X_valid = np.array(X_batch)
-        y_valid = np.array(y_batch)
+            X_valid.append(randomize_brightness(crop_and_resize(images[choice])))
+            y_valid.append(angle[choice])
+        X_valid = np.array(X_valid)
+        y_valid = np.array(y_valid)
         yield X_valid, y_valid
 
-for i in range(10):
-    gen = generator_images(32)
-    x_b, y_b = next(gen)
-    print(x_b.shape, y_b.shape)
+from keras.models import Sequential
+from keras.layers import Convolution2D, Dropout, MaxPooling2D, Flatten, Activation, Dense, Cropping2D, Lambda
 
-# from keras.models import Sequential
-# from keras.layers import Convolution2D, Dropout, MaxPooling2D, Flatten, Activation, Dense, Cropping2D, Lambda
+model = Sequential()
+model.add(Lambda(lambda x: x /255.0 - 0.5, input_shape=(64,64,3) ))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation = 'relu'))
+# model.add(Dropout(0.2))
+# model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation = 'relu'))
+# model.add(Dropout(0.2))
+# model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation = 'relu'))
+# model.add(Dropout(0.2))
+# model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Flatten())
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
 
-# model = Sequential()
-# model.add(Lambda(lambda x: x /255.0 - 0.5, input_shape=(64,64,3) ))
-# model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation = 'relu'))
-# # model.add(Dropout(0.2))
-# # model.add(MaxPooling2D(pool_size = (2,2)))
-# model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation = 'relu'))
-# # model.add(Dropout(0.2))
-# # model.add(MaxPooling2D(pool_size = (2,2)))
-# model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation = 'relu'))
-# # model.add(Dropout(0.2))
-# # model.add(MaxPooling2D(pool_size = (2,2)))
-# model.add(Flatten())
-# model.add(Dense(50))
-# model.add(Dense(10))
-# model.add(Dense(1))
+model.compile('adam', 'mse')
+model.fit_generator(generator_images(32), samples_per_epoch = len(X_train), nb_epoch = 2, validation_data=generator_valid(32), nb_val_samples=int(len(X_train)*0.2))
 
-# model.compile('adam', 'mse')
-# model.fit_generator(generator_images(lines, 32), samples_per_epoch = 32144, nb_epoch = 2, validation_data=generator_valid(lines), nb_val_samples=6400)
-
-# model.save('model.h5')
+model.save('model.h5')
